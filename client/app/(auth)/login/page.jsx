70f42b929fcf0a0b6e890718c2732c1b0054b7da
Mohamed Yahia <m.yahya199290@gmail.com>
2025-03-04 00:07:48 +0300

    addig githob provider and facebook

--- client/app/(auth)/login/page.jsx
+++ client/app/(auth)/login/page.jsx
@@ -1,16 +1,16 @@
 "use client"
 import { firebase_auth } from "@/utils/firebase_config";
 import axios from "axios";
-import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { FacebookAuthProvider, GithubAuthProvider, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { FcGoogle } from "react-icons/fc";
 
 export default function Login () {
 
     console.log(process.env.NEXT_PUBLIC_API_URL)
 
-    const handle_sigin =  async()=> {
-        const provider = new GoogleAuthProvider();
-        const {user: {email, displayName: user_name,photoURL: profile_image_url}} = await signInWithPopup(firebase_auth,provider);
+    const handle_sigin =  async(provider)=> {
+        const auth_provider = provider === 'google' ? new GoogleAuthProvider() : provider === 'githob' ? new GithubAuthProvider() : new FacebookAuthProvider()
+        const {user: {email, displayName: user_name,photoURL: profile_image_url}} = await signInWithPopup(firebase_auth,auth_provider);
         console.log(user_name,profile_image_url,email);
 
         try {
@@ -27,15 +27,35 @@ export default function Login () {
         }
     };
 
-    return (
-        <div className="w-screen h-screen flex justify-center items-center">
+    const SignInBTN = ({provider,Icon})=> {
+        return (
             <button 
                 className="flex justify-center items-center gap-2 px-3 py-1 rounded-sm bg-emerald-200 cursor-pointer"
                 onClick={handle_sigin}
                 >
-                <FcGoogle />
-                <span>signin with google</span>
+                <Icon/>
+                <span>{`sigin with ${provider}`}</span>
             </button>
+        )
+    }
+
+    return (
+        <div className="w-screen h-screen flex justify-center items-center">
+            <SignInBTN 
+                provider='google' 
+                Icon={FcGoogle} 
+                handle_click={()=> handle_sigin('google')} 
+                />
+            <SignInBTN 
+                provider='githob' 
+                Icon={FcGoogle} 
+                handle_click={()=> handle_sigin('githob')} 
+                />
+             <SignInBTN 
+                provider='facebook' 
+                Icon={FcGoogle} 
+                handle_click={()=> handle_sigin('facebook')} 
+                />
         </div>
     )
 }
