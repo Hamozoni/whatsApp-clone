020d5b42a4c96bec3493264906505e7ee70facf5
Mohamed Yahia <m.yahya199290@gmail.com>
2025-03-04 13:42:57 +0300

    handling Error (auth/account-exists-with-different-credential

--- client/app/(auth)/login/page.jsx
+++ client/app/(auth)/login/page.jsx
@@ -1,37 +1,44 @@
 "use client"
 import { firebase_auth } from "@/utils/firebase_config";
 import axios from "axios";
-import { FacebookAuthProvider, GithubAuthProvider, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { GithubAuthProvider, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { FcGoogle } from "react-icons/fc";
+import { FaGithub } from "react-icons/fa";
 
 export default function Login () {
 
-    console.log(process.env.NEXT_PUBLIC_API_URL)
-
     const handle_sigin =  async(provider)=> {
-        const auth_provider = provider === 'google' ? new GoogleAuthProvider() : provider === 'githob' ? new GithubAuthProvider() : new FacebookAuthProvider()
-        const {user: {email, displayName: user_name,photoURL: profile_image_url}} = await signInWithPopup(firebase_auth,auth_provider);
-        console.log(user_name,profile_image_url,email);
+        console.log(provider)
+        const auth_provider = provider === 'google' ? new GoogleAuthProvider() :  new GithubAuthProvider() 
+        await signInWithPopup(firebase_auth,auth_provider)
+        .then(async({user})=> {
+            console.log(user)
 
-        try {
-            if(email) {
-
-                const {data} = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/auth/find_user`,{email})
-
-                console.log(data)
-
+            const {email, displayName: user_name,photoURL: profile_image_url} = user;
+            try {
+                if(email) {
+    
+                    const {data} = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/auth/find_user`,{email})
+    
+                    console.log(data)
+    
+                }
             }
-        }
-        catch (error){
+            catch (error){
+                console.log(error)
+            }
+        })
+        .catch((error)=> {
             console.log(error)
-        }
+        })
+
     };
 
     const SignInBTN = ({provider,Icon})=> {
         return (
             <button 
                 className="flex justify-center items-center gap-2 px-3 py-1 rounded-sm bg-emerald-200 cursor-pointer"
-                onClick={handle_sigin}
+                onClick={()=> handle_sigin(provider)}
                 >
                 <Icon/>
                 <span>{`sigin with ${provider}`}</span>
@@ -40,21 +47,14 @@ export default function Login () {
     }
 
     return (
-        <div className="w-screen h-screen flex justify-center items-center">
+        <div className="w-screen h-screen flex justify-center gap-2 items-center flex-col">
             <SignInBTN 
                 provider='google' 
                 Icon={FcGoogle} 
-                handle_click={()=> handle_sigin('google')} 
                 />
             <SignInBTN 
-                provider='githob' 
-                Icon={FcGoogle} 
-                handle_click={()=> handle_sigin('githob')} 
-                />
-             <SignInBTN 
-                provider='facebook' 
-                Icon={FcGoogle} 
-                handle_click={()=> handle_sigin('facebook')} 
+                provider='github' 
+                Icon={FaGithub} 
                 />
         </div>
     )
