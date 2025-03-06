7cf9f2d0a0b2535b8f31004050e67c13b1eb9a8a
Mohamed Yahia <m.yahya199290@gmail.com>
2025-03-06 10:56:01 +0300

    creating auth context

---
+++ client/components/auth/context.jsx
@@ -0,0 +1,17 @@
+"use client"
+import { createContext } from "react";
+
+const Auth_context = createContext();
+
+
+const Auth_context_provider = ()=> {
+
+    return (
+        <Auth_context.Provider>
+
+        </Auth_context.Provider>
+    )
+};
+
+export default Auth_context_provider;
+
