042cb86b3cb6238891acf997b84c329ca59c3310
Mohamed Yahia <m.yahya199290@gmail.com>
2025-03-10 19:37:11 +0300

    creating chat schema

---
+++ server/models/chat.js
@@ -0,0 +1,7 @@
+
+
+import mongoose from "mongoose";
+
+const CHAT_SCHEMA = new mongoose.Schema({
+
+})
--- server/models/user.js
+++ server/models/user.js
@@ -5,8 +5,8 @@ const USER_SCHEMA = new mongoose.Schema({
     email: {type: String,require: true},
     is_online: {type: Boolean, default: false },
     profile_picture: {type: String,require: false},
-    chats: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Chat' }],
     contacts: [{type: mongoose.Schema.Types.ObjectId, ref: 'User'}],
-    created_at: {type: Date, default: now()},
-    updated_at: {type: Date, default: now()}
-})
+    updated_at: {type: Date, default: Date.now}
+},{timestamps: true});
+
+export const User = mongoose.model('User'.USER_SCHEMA);
