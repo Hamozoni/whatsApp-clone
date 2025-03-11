eac38824f5355e3400dbfd1b05d2613f0b86e6d5
Mohamed Yahia <m.yahya199290@gmail.com>
2025-03-11 14:41:12 +0300

    crating status schema

--- server/models/message.js
+++ server/models/message.js
@@ -1,9 +1,19 @@
 import mongoose from "mongoose";
 
 const MESSAGE_SCHEMA = new mongoose.Schema({
-    sender: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
+    chat: { type: mongoose.Schema.Types.ObjectId, ref: 'Chat' },
+    sender: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    text: { type: String },
+    media: { type: String },
+    type: {type: String,enum: ['TEXT', 'AUDIO', 'PHOTO','VIDEO','DOCUMENT'], 
+        default: 'SENT'},
+    status: { 
+        type: String, 
+        enum: ['SENT', 'DELIVERED', 'READ'], 
+        default: 'SENT' 
+      }
 
-});
+},{timestamps: true});
 
 const Message = mongoose.model('Message',MESSAGE_SCHEMA);
 
---
+++ server/models/status.js
@@ -0,0 +1,5 @@
+import mongoose from "mongoose";
+
+const STATUS_SCHEMA = new mongoose.Schema({
+    
+})
