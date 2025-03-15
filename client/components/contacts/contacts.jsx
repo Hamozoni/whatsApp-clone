a015d41ff7ad8d550b155e5162543c2c91e6de32
Mohamed Yahia <m.yahya199290@gmail.com>
2025-03-15 13:48:45 +0300

    client/components/contacts/contacts.jsx

--- client/components/contacts/contacts.jsx
+++ client/components/contacts/contacts.jsx
@@ -4,13 +4,71 @@ import { FaArrowLeftLong } from "react-icons/fa6";
 import { Search_form } from "../inputs/search_form";
 import { useState } from "react";
 import { GrGroup,GrUserAdd } from "react-icons/gr";
+import Image from "next/image";
+
+const contacts = [
+    {
+        _id: 1,
+        name: 'Ahmed Hamed',
+        email: 'ahmed122@gmail.com',
+        profile_picture: '/placeholder_avatar.jpg'
+    },
+    {
+        _id: 2,
+        name: 'osama ahmed',
+        email: 'osama@gmail.com',
+        profile_picture: '/placeholder_avatar.jpg'
+    },
+    {
+        _id: 3,
+        name: 'reem mohamed',
+        email: 'reemmo@gmail.com',
+        profile_picture: '/placeholder_avatar.jpg'
+    },
+    {
+        _id: 4,
+        name: 'Ahmed Hamed',
+        email: 'ahmed122@gmail.com',
+        profile_picture: '/placeholder_avatar.jpg'
+    },
+    {
+        _id: 5,
+        name: 'osama ahmed',
+        email: 'osama@gmail.com',
+        profile_picture: '/placeholder_avatar.jpg'
+    },
+    {
+        _id: 6,
+        name: 'reem mohamed',
+        email: 'reemmo@gmail.com',
+        profile_picture: '/placeholder_avatar.jpg'
+    },
+    {
+        _id: 7,
+        name: 'Ahmed Hamed',
+        email: 'ahmed122@gmail.com',
+        profile_picture: '/placeholder_avatar.jpg'
+    },
+    {
+        _id: 8,
+        name: 'osama ahmed',
+        email: 'osama@gmail.com',
+        profile_picture: '/placeholder_avatar.jpg'
+    },
+    {
+        _id: 10,
+        name: 'reem mohamed',
+        email: 'reemmo@gmail.com',
+        profile_picture: '/placeholder_avatar.jpg'
+    },
+]
 
 const Button = ({Icon,text,handle_cleck})=> {
 
     return (
         <button onClick={handle_cleck} className="cursor-pointer flex items-center gap-3 w-full px-3 hover:bg-[#222e35]">
-             <div className="p-2 bg-emerald-600 flex items-center justify-center rounded-full">
-                <Icon size={22} />
+             <div className="p-3 my-2 bg-emerald-600 flex items-center justify-center rounded-full">
+                <Icon size={26} />
              </div>
              <div className="flex items-center border-b flex-1 border-b-[#222e35] py-3">
                 <h5>{text}</h5>
@@ -24,8 +82,8 @@ export const Contacts = ({set_is_contact})=> {
     const [search_value,set_search_value] = useState('');
 
     return (
-        <div className="">
-            <header className="px-3 py-5">
+        <div className="h-screen max-h-screen overflow-y-auto">
+            <header className="px-3 py-5 sticky top-0 bg-[#111b21]">
                 <div className="mb-6 flex items-center gap-6">
                     <button className=" cursor-pointer" onClick={()=> set_is_contact(false)}>
 
@@ -35,13 +93,31 @@ export const Contacts = ({set_is_contact})=> {
                 </div>
                 <Search_form  value={search_value} set_value={set_search_value} handle_search={_=> ''}/>
             </header>
-            <div className="h-[calc(100vh - 130px)] overflow-y-auto">
-                <div className="h-fit">
-                    <Button Icon={GrGroup} text='new group' handle_cleck={()=> ''} />
-                    <Button Icon={GrUserAdd} text='new contact' handle_cleck={()=> ''} /> 
+            <div className="">
+                <Button Icon={GrGroup} text='new group' handle_cleck={()=> ''} />
+                <Button Icon={GrUserAdd} text='new contact' handle_cleck={()=> ''} /> 
 
-                     <h6>contacts on whatsapp</h6>
-
+                    <h6 className="px-5 py-3 text-xs text-emerald-300">
+                    contacts on whatsapp
+                </h6>
+                <div className="">
+                    {
+                        contacts?.map(({_id,name,email,profile_picture})=> (
+                            <div key={_id} className="cursor-pointer flex items-center gap-3 w-full px-3 hover:bg-[#222e35]">
+                                <Image 
+                                    className="flex items-center justify-center rounded-full"
+                                    src={profile_picture} 
+                                    width={50} 
+                                    height={50} 
+                                    alt="avatar"
+                                />
+                                    <div className="flex flex-col border-b flex-1 border-b-[#222e35] py-3">
+                                    <h5>{name}</h5>
+                                    <span className="text-xs font-light text-gray-400">{email}</span>
+                                </div>
+                            </div>
+                        ))
+                    }
                 </div>
             </div>
 
