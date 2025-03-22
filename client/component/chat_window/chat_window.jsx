60a4163911e718661149fe8ad8532fae5b7396ca
Mohamed Yahia <m.yahya199290@gmail.com>
2025-03-22 22:01:22 +0300

    modified:   client/component/chat_window/chat_window.jsx

--- client/component/chat_window/chat_window.jsx
+++ client/component/chat_window/chat_window.jsx
@@ -7,6 +7,7 @@ import { User_context } from '../context';
 import Image from 'next/image';
 import axios from 'axios';
 import { Message_card } from './message_card';
+import Loading from '@/app/loading';
 
 
 const Chat_window = () => {
@@ -14,24 +15,25 @@ const Chat_window = () => {
     const {user,active_chat} = useContext(User_context);
     const [messages, set_messages] = useState([]);
     const [receiver, set_receiver] = useState(null);
+    const [is_loading, set_is_loading] = useState(true);
+    const [error, set_error] = useState(null);
     
     useEffect(() => {
 
       set_receiver(active_chat?.members?.filter(e=> e._id !== user?._id)[0]);
 
-      console.log(active_chat?.members?.filter(e=> e._id !== user?._id)[0])
-
       const fetch_messages = async ()=> {
+        set_is_loading(true);
+        set_error(null)
         try{
-  
           const {data} = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/message?chat_id=${active_chat?._id}`);
-  
-          console.log(data);
-  
           set_messages(data?.messages)
         }
         catch (error) {
-  
+          set_error(error?.message)
+        }
+        finally {
+          set_is_loading(false)
         }
 
       };
@@ -55,7 +57,9 @@ const Chat_window = () => {
           <div className="flex-1 overflow-y-auto p-4 bg-[#111b21] bg-opacity-60 bg-chat-pattern hide_model">
               <div className="space-y-2 text-[#f7f8fa]">
                 {
-                  messages.map(message => (
+                  is_loading ? 
+                  <Loading />
+                  : messages.map(message => (
                     <Message_card 
                       key={message?._id} 
                       message={message} 
