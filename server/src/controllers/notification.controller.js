011ec976d6b6f18bbe8c870a0465f74b98ba9202
Mohamed Yahia <m.yahya199290@gmail.com>
2025-03-24 19:25:08 +0300

    server/src/routes/notification.route.js

--- server/src/controllers/notification.controller.js
+++ server/src/controllers/notification.controller.js
@@ -11,10 +11,6 @@ export const post_notification = async (req,res,next)=> {
 
     try {
 
-        if(unreaded_messages && missed_calls) {
-            const notification = await Notification.create(req.body);
-            return res.json({message: 'notification created',status: true,notification})
-        }
         if(unreaded_messages) {
             const notification = await Notification.create({user_email,unreaded_messages});
             return res.json({message: 'notification created',status: true,notification})
@@ -41,6 +37,37 @@ export const get_notification = async (req,res,next)=> {
     try {
         const notification = await Notification.findOne({user_email});
         return res.json({message: 'notification found',status: true,notification})
+    }
+    catch (error){
+        next(error);
+    }
+};
+
+export const put_notification = async (req,res,next)=> {
+
+    const {user_email,unreaded_messages,missed_calls} = req.body;
+
+    if(!user_email) {
+        return res.json({message: 'user email is missing',status: false})
+    };
+
+    try {
+
+        if(unreaded_messages) {
+
+            const notification = await Notification.findOneAndUpdate({user_email},{$addToSet: {
+                unreaded_messages
+            }},{new: true});
+            return res.json({message: 'notification created',status: true,notification})
+        }
+        if(missed_calls) {
+
+            const notification = await Notification.findOneAndUpdate({user_email},{$addToSet: {
+                missed_calls
+            }},{new: true});
+            return res.json({message: 'notification created',status: true,notification})
+        }
+
     }
     catch (error){
         next(error);
--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -2,12 +2,13 @@ import { Router } from 'express';
 import message_route from './message.route.js';
 import contact_route from './contact.route.js';
 import user_route from './user.route.js';
+import notification_route from './notification.route.js'
 
 const router = Router();
 
 router.use('/message',message_route);
 router.use('/user',user_route);
-// router.use('/chat',chat_route);
+router.use('/notification',notification_route);
 router.use('/contact',contact_route);
 
 export default router;
