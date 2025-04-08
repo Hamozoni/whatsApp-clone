173a65e0281e698f1d16ff09f08b6e814b7dbf6e
Mohamed Yahia <m.yahya199290@gmail.com>
2025-04-08 13:06:43 +0300

    client/components/ui/camera_photo.jsx

---
+++ client/components/ui/camera_photo.jsx
@@ -0,0 +1,32 @@
+"use client"
+import { useEffect, useRef, useState } from "react"
+
+
+export const Camera_photo = ({video_src})=> {
+
+
+    const [is_photo,set_is_photo] = useState(false);
+    const [photo,set_photo] = useState(null);
+
+    const video_ref = useRef(null);
+
+    useEffect(()=> {
+        video_ref.current.srcObject = video_src
+    },[]);
+
+
+
+    return (
+        <div className=" absolute left-2 bottom-0 translate-y-[102%] w-full z-50">
+            <header><span>cansel</span></header>
+            <div className="min-w-full min-h-[calc(100vh - 400px)] ">
+                {
+                    is_photo ?
+                    <img className="w-full" src="" alt="" />
+                    :
+                    <video ref={video_ref} className="w-full min-w-[500px] min-h-[550px]" autoPlay muted></video>
+                }
+            </div>
+        </div>
+    )
+}
--- client/components/ui/chose_document.jsx
+++ client/components/ui/chose_document.jsx
@@ -6,76 +6,89 @@ import {
     MdOutlineVideoCall,MdOutlineLinkedCamera,
     MdOutlinePermContactCalendar 
 } from "react-icons/md";
+import { Camera_photo } from "./camera_photo";
 
 
-const documents = [
-    {
-        id: 1,
-        name: 'document',
-        Icon: IoDocumentTextOutline,
-        type: 'file',
-    },
-    {
-        id: 2,
-        name: 'photos',
-        Icon: MdOutlineAddPhotoAlternate,
-        type: 'flie',
-    },
-    {
-        id: 3,
-        name: 'videos',
-        Icon: MdOutlineVideoCall,
-        type: 'flie',
-    },
-    ,
-    {
-        id: 4,
-        name: 'camera',
-        Icon: MdOutlineLinkedCamera,
-        type: 'camera',
-    },
-    {
-        id: 5,
-        name: 'contact',
-        Icon: MdOutlinePermContactCalendar,
-        type: 'contact',
-    }
-]
+// const documents = [
+//     {
+//         id: 1,
+//         name: 'document',
+//         Icon: IoDocumentTextOutline,
+//         type: 'file',
+//     },
+//     {
+//         id: 2,
+//         name: 'photos',
+//         Icon: MdOutlineAddPhotoAlternate,
+//         type: 'flie',
+//     },
+//     {
+//         id: 3,
+//         name: 'videos',
+//         Icon: MdOutlineVideoCall,
+//         type: 'flie',
+//     },
+//     ,
+//     {
+//         id: 4,
+//         name: 'camera',
+//         Icon: MdOutlineLinkedCamera,
+//         type: 'camera',
+//     },
+//     {
+//         id: 5,
+//         name: 'contact',
+//         Icon: MdOutlinePermContactCalendar,
+//         type: 'contact',
+//     }
+// ]
 
 export const Chose_document = () => {
 
 
-    const [avatar,set_avatar]= useState()
-    const handle_vatar = (e) => {
-        const file = e.target.files?.[0];
-        if (file) {
-          const reader = new FileReader();
-          reader.onloadend = () => {
-            set_avatar(reader?.result);
-            console.log(reader)
-          };
-          reader.readAsDataURL(file);
-        }
-      };
+    const [video_src,set_video_src]= useState(null);
+    // const handle_vatar = (e) => {
+    //     const file = e.target.files?.[0];
+    //     if (file) {
+    //       const reader = new FileReader();
+    //       reader.onloadend = () => {
+    //         set_avatar(reader?.result);
+    //         console.log(reader)
+    //       };
+    //       reader.readAsDataURL(file);
+    //     }
+    //   };
+
+    const handle_camera = async ()=> {
+       const stream = await navigator.mediaDevices.getUserMedia({video: true, audio: true})
+            set_video_src(stream);
+
+    }
     
     return (
         <div className=" absolute left-2 top-0 -translate-y-[103%] bg-[#222e35] p-3 rounded-md">
-            <ul>
-                {
+            <ul className=" relative">
+                {/* {
                     documents?.map(({id,name,Icon})=> (
-                        <li  key={id} className="">
-                            <label className="flex items-center gap-3 text-[#f7f8fa] rounded-md hover:bg-[#2b3c46] min-w-[150px] capitalize p-2">
-                            <input type="file"
-                                accept="document/pdf"
-                                onChange={handle_vatar}
-                                className="hidden" />
-                                <Icon size={24}/>
-                               <span>{name}</span> 
-                            </label>
-                        </li>
-                    ))
-                }
 
+                    ))
+                } */}
+                 <li 
+                     onClick={handle_camera} 
+                    className="flex items-center gap-3 text-[#f7f8fa] rounded-md hover:bg-[#2b3c46] min-w-[150px] capitalize p-2">
+                    {/* <label className="">
+                        <input type="file"
+                            accept="document/pdf"
+                            onChange={handle_vatar}
+                            className="hidden" />
+                        </label> */}
+                            <MdOutlineLinkedCamera size={24}/>
+                            <span>camera</span> 
+                  </li>
+
+            {
+                video_src && <Camera_photo video_src={video_src} />
+            }
             </ul>
         </div>
     )
