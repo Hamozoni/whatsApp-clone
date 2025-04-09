797fbdc5f69869189490b6da79b1f17507a5a70f
Mohamed Yahia <m.yahya199290@gmail.com>
2025-04-09 12:24:53 +0300

    client/hooks/use_fetch.jsx

--- client/components/chat_window/active_chat.jsx
+++ client/components/chat_window/active_chat.jsx
@@ -2,106 +2,94 @@
 "use client";
 import React, { useContext, useEffect, useRef, useState } from 'react';
 import { User_context } from '../../contexts/user.context';
-import axios from 'axios';
 import { Message_card } from './message_card';
 import { Loading_component } from '../ui/loading_component';
 import { useSocket } from '@/hooks/useSocket';
 import update_message_status from '@/utils/update_mesages_status';
+import { Use_fetch } from '@/hooks/use_fetch';
+import { Chat_window_context } from '@/contexts/chat_window.context';
+
+const className = 'flex-1 overflow-y-auto space-y-2 p-4 bg-[#111b21] bg-opacity-60 bg-chat-pattern hide_model'
 
 const Active_chat = () => {
 
-    
+  
   const {user,active_chat} = useContext(User_context);
-  
-    const [messages, set_messages] = useState([]);
-    const [receiver, set_receiver] = useState(null);
-    const [is_loading, set_is_loading] = useState(false);
-    const [error, set_error] = useState(null);
-    const socket = useSocket();
-    const sound_ref = useRef(null);
-  
+  const {messages,set_messages} = useContext(Chat_window_context);
 
-    const fetch_messages = async ()=> {
-        
-      set_is_loading(true);
-      set_error(null);
-      try {
+  if(!active_chat._id) {
+    return (
+      <div className={className}>
+         <h4>no active_chat._id</h4>
+      </div>
+    )
+  }
+  const {data,loading,error} = Use_fetch({end_point:`/message?chat_id=${active_chat?._id}`});
 
-        const {data} = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/message?chat_id=${active_chat?._id}`);
+  const socket = useSocket();
+  const sound_ref = useRef(null);
 
-        if(data?.status) {
-            set_messages(data?.messages);
-        }
-        else {
-          set_error(data?.message)
-        }
-      }
-      catch (error) {
-        set_error(error?.message)
-      }
-      finally {
-        set_is_loading(false)
-      }
-    };
+  useEffect(()=> {
+    console.log(data,loading,error)
+    set_messages(data?.messages);
+  },[loading]);
 
-    useEffect(() => {
-      set_messages([]);
-      const receiver = active_chat.members.filter(e=> e._id !== user?._id)[0]
-      set_receiver(receiver);
-      
-      if(!active_chat?._id) return
-        fetch_messages();
+
+  useEffect(() => {
+
+    socket?.emit('join_room',active_chat?._id);
+
+    socket?.on('send_message',chat => {
+      if(chat?.last_message?.sender !== user?._id) {
+        set_messages(prev=> [...prev,chat?.last_message]);
         update_message_status(socket,active_chat?._id,receiver?._id,'READ');
-    },[active_chat?._id]);
-    
-    
-    useEffect(() => {
-      if(!active_chat?._id) return;
-
-      socket?.emit('join_room',active_chat?._id);
-      socket?.on('send_message',chat => {
-
-        if(chat?.last_message?.sender !== user?._id) {
-          set_messages(prev=> [...prev,chat?.last_message]);
-          update_message_status(socket,active_chat?._id,receiver?._id,'READ');
-          
-          sound_ref.current = new Audio('./new_message_sound_2.mp3')
-          sound_ref.current.play();
-        }
-
-      });
-
-
-      socket?.on('message_status_changed', data => {
-        set_messages(prev=> {
-          let new_messages = [];
-          prev?.forEach( e => {
-            if(e.sender === data?.sender && e.status !== 'READ') {
-              new_messages.push({...e,status: data?.status})
-            }else {
-              new_messages.push(e)
-            }
-          });
-
-          return [...new_messages]
-        })
-      });
-
-      return ()=> {
-        socket?.off('send_message');
-        socket?.off('message_status_changed');
+        sound_ref.current = new Audio('./new_message_sound_2.mp3')
+        sound_ref.current.play();
       }
-      
-  },[socket,active_chat]);
 
+    });
+
+
+    socket?.on('message_status_changed', data => {
+      set_messages(prev=> {
+        let new_messages = [];
+        prev?.forEach( e => {
+          if(e.sender === data?.sender && e.status !== 'READ') {
+            new_messages.push({...e,status: data?.status})
+          }else {
+            new_messages.push(e)
+          }
+        });
+
+        return [...new_messages]
+      })
+    });
+
+    return ()=> {
+      socket?.off('send_message');
+      socket?.off('message_status_changed');
+    }
+    
+},[socket]);
+  
+
+  if(error){
+    return (
+      <div className={className}>
+        <h3>{error}</h3>
+      </div>
+    )
+  }
 
   return (
               
-      <div className="flex-1 overflow-y-auto space-y-2 p-4 bg-[#111b21] bg-opacity-60 bg-chat-pattern hide_model">
-            <>
+      <div className={className}>
             {
-                is_loading ? 
+                loading ? 
+                <div className={className}>
                   <Loading_component />
+
+                </div>
                   : messages?.map(message => (
                     <Message_card 
                       key={message?._id} 
@@ -110,11 +98,6 @@ const Active_chat = () => {
                     />
                 ))
               }
-              <div className="w-full h-[2px]">
-                  
-              </div>
-            </>  
-              
      </div> 
   );
 };
