b9752d8018eb909b390ffbeee85c2a81dba667e3
Mohamed Yahia <m.yahya199290@gmail.com>
2025-04-22 17:34:01 +0300

    modified:   client/components/chat_window/media_capture.jsx

--- client/components/chat_window/media_capture.jsx
+++ client/components/chat_window/media_capture.jsx
@@ -1,12 +1,17 @@
 "use client"
-import { useEffect, useRef, useState } from "react";
+import { Chat_window_context } from "@/contexts/chat_window.context";
+import { useContext, useEffect, useRef, useState } from "react";
 
 
 export const Media_capture = ()=> {
 
+    const {set_is_camera} = useContext(Chat_window_context)
+
 
     const [is_photo,set_is_photo] = useState(false);
     const [photo,set_photo] = useState(null);
+    const [video_src,set_video_src] = useState(null);
+    const [camera_user_mode,set_camera_user_mode] = useState(true);
     const canvas_ref = useRef()
 
     const video_ref = useRef(null);
@@ -16,10 +21,11 @@ export const Media_capture = ()=> {
             try {
                 const stream = await navigator.mediaDevices.getUserMedia({
                     audio: true, 
-                    video:{facingMode: 'user'}
+                    video:{facingMode: camera_user_mode ? 'user':'environment'}
                 });
 
                 if(video_ref?.current) {
+                    set_video_src(stream)
                     video_ref.current.srcObject = stream
                 }
             }
@@ -27,7 +33,14 @@ export const Media_capture = ()=> {
 
             }
         };
-        initialize_camera()
+        initialize_camera();
+
+        return ()=> {
+            if(video_ref?.current?.srcObject) {
+                const tracks = video_src.getTracks();
+                tracks.forEach(track=> track.stop())
+            }
+        }
         
     },[]);
 
@@ -36,7 +49,7 @@ export const Media_capture = ()=> {
     return (
         <div className="fixed  left-0 top-0 w-full h-full z-30">
             <header>
-                <span>cansel</span>
+                <button onClick={()=> set_is_camera(false)}>cansel</button>
             </header>
             <div className="min-w-full min-h-[calc(100vh - 400px)] ">
                 {
