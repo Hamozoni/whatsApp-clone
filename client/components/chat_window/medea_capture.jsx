fa37bb73b4b49baf5d36111761c4dba03a456ede
Mohamed Yahia <m.yahya199290@gmail.com>
2025-04-22 16:48:33 +0300

    modified:   client/components/chat_window/medea_capture.jsx

--- client/components/chat_window/medea_capture.jsx
+++ client/components/chat_window/medea_capture.jsx
@@ -1,5 +1,5 @@
 "use client"
-import { useEffect, useRef, useState } from "react"
+import { useEffect, useRef, useState } from "react";
 
 
 export const Medea_capture = ()=> {
@@ -18,15 +18,25 @@ export const Medea_capture = ()=> {
 
     return (
         <div className="w-full h-full">
-            <header><span>cansel</span></header>
+            <header>
+                <span>cansel</span>
+            </header>
             <div className="min-w-full min-h-[calc(100vh - 400px)] ">
                 {
                     is_photo ?
                     <img className="w-full" src="" alt="" />
                     :
-                    <video ref={video_ref} className="w-full min-w-[500px] min-h-[550px]" autoPlay muted></video>
+                    <div className="">
+                        <video ref={video_ref} className="w-full min-w-[500px] min-h-[300px]" autoPlay muted></video>
+                        <div className="controls">
+                            <button onClick={capturePhoto}>Take Photo</button>
+                            <button onClick={startRecording}>Start Recording</button>
+                            <button onClick={stopRecording}>Stop Recording</button>
+                        </div>
+                    </div>
                 }
             </div>
+            <canvas ref={canvas_ref} className="hidden" />
         </div>
     )
 }
