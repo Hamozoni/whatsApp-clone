56bc33a97fd20778324ae3d5a88ab657d5f03370
Mohamed Yahia <m.yahya199290@gmail.com>
2025-04-24 19:11:49 +0300

    modified:   client/components/call/video_call.jsx

--- client/components/call/video_call.jsx
+++ client/components/call/video_call.jsx
@@ -1,6 +1,12 @@
+"use client";
 
+import { useRef, useState } from "react";
 
 export const Video_call = ()=> {
+    const [pc,set_pc] = useState({});
+   const local_video_ref = useRef(null);
+   const remote_video_ref = useRef(null);
+   const peers_ref = useRef(null);
     return (
         <div className="">
             
