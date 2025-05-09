7c77a2f216c1abd091e3deb9c7d0df28c1d917fc
Mohamed Yahia <m.yahya199290@gmail.com>
2025-05-09 14:22:42 +0300

    modified:   client/src/components/status/status.jsx         modified:   client/src/components/status/update_status_model.jsx

--- client/src/components/status/status.jsx
+++ client/src/components/status/status.jsx
@@ -1,36 +1,58 @@
-import { useContext } from "react"
+import { useContext, useState } from "react"
 import { User_context } from "../../contexts/user.context";
 import { GrFormAdd } from "react-icons/gr";
+import { Update_status_model } from "./update_status_model";
+import { Close_model } from "../ui/close_model";
 
 
 export const Status = ()=> {
 
-    const {user} = useContext(User_context)
+    const {user} = useContext(User_context);
+    const [is_update,set_is_update] = useState(false);
+
+
+
+
+
+
+
     return (
         <div className="flex h-dvh">
-            <div className="p-3 flex-1 border-r border-cyan-950">
-                <div className="flex items-center justify-between">
-                    <h4 className="text-xl font-bold mb-3">Status</h4>
-                    <button className="rounded-full border border-white">
-                        <GrFormAdd size={20}/>
-                    </button>
-                </div>
-                <div className="flex gap-2 cursor-pointer">
-                    <div className="relative">
-                        <img 
-                            src={user?.profile_picture} 
-                            alt="avatar" 
-                            className="w-11 h-11 rounded-full"
-                            />
-                            <button className=" absolute right-0 bottom-0 rounded-full border border-cyan-950 bg-emerald-500">
-                                <GrFormAdd />
-                            </button>
+            <div className=" p-3 flex-1 border-r border-cyan-950">
+                <header className="relative h-fit">
+                    <div className="flex items-center justify-between">
+                        <h4 className="text-xl font-bold mb-3">Status</h4>
+                        <button onClick={()=> set_is_update(!is_update)} className="rounded-full border border-white">
+                            <GrFormAdd size={20}/>
+                        </button>
                     </div>
-                    <div className="">
-                        <h5 className="m-0">My status</h5>
-                        <span className="text-xs">Click to add status update</span>
+                    <div onClick={()=> set_is_update(!is_update)} className="flex gap-2 cursor-pointer">
+                        <div className="relative">
+                            <img 
+                                src={user?.profile_picture} 
+                                alt="avatar" 
+                                className="w-11 h-11 rounded-full"
+                                />
+                                <button className=" absolute right-0 bottom-0 rounded-full border border-cyan-950 bg-emerald-500">
+                                    <GrFormAdd />
+                                </button>
+                        </div>
+                        <div className="">
+                            <h5 className="m-0">My status</h5>
+                            <span className="text-xs">Click to add status update</span>
+                        </div>
                     </div>
-                </div>
+                    {
+                        is_update && 
+                        <>
+                            <Close_model set_model={set_is_update} />
+                            <Update_status_model />
+
+                        </>
+                    
+                    }
+
+                </header>
             </div>
             <div className="flex-2"></div>
         </div>
--- client/src/components/status/update_status_model.jsx
+++ client/src/components/status/update_status_model.jsx
@@ -3,11 +3,11 @@ import { TbLibraryPhoto } from "react-icons/tb";
 import { LiaPencilAltSolid } from "react-icons/lia";
 export const Update_status_model = ()=> {
     return (
-        <div className="">
-            <button className="flex items-center gap-3 rounded-xl ">
+        <div className="w-[200px] absolute -bottom-full left-3 z-20 bg-[#132730] rounded-xl p-2">
+            <button className="w-full flex items-center gap-3 rounded-xl p-2 hover:bg-[#252c30]">
                 <TbLibraryPhoto /> <span>Photos & videos</span>
             </button>
-            <button className="flex items-center gap-3 rounded-xl ">
+            <button className="w-full flex items-center gap-3 rounded-xl p-2 hover:bg-[#252c30] ">
                 <LiaPencilAltSolid /> <span>Text</span>
             </button>
         </div>
