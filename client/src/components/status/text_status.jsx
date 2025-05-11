0579d2532ede661ce429c7f72ac3141df20c1399
Mohamed Yahia <m.yahya199290@gmail.com>
2025-05-11 22:42:48 +0300

    modified:   client/src/components/status/text_status.jsx

--- client/src/components/status/text_status.jsx
+++ client/src/components/status/text_status.jsx
@@ -53,7 +53,7 @@ export const Text_status = ({set_status_type})=> {
                 </div>
             </header>
             <div className="p-3 flex items-center justify-center">
-                <p className="text-6xl text-[#999] font-serif">Type a status</p>
+                <p style={{fontStyle:font[0]}} className={`text-6xl text-[#999]`}>Type a status</p>
             </div>
             <footer className="bg-[#0000003a] h-20 flex items-center justify-between px-3">
                 <button className="flex items-center gap-1 bg-[#00000065] py-2 px-6 rounded-3xl">
