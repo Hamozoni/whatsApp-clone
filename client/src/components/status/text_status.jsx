b6660a78e7db63684861a988839566d0f9053f9e
Mohamed Yahia <m.yahya199290@gmail.com>
2025-05-11 21:32:26 +0300

    modified:   client/src/components/status/text_status.jsx

--- client/src/components/status/text_status.jsx
+++ client/src/components/status/text_status.jsx
@@ -1,5 +1,6 @@
-import { IoCloseSharp,IoColorPaletteSharp } from "react-icons/io5";
+import { IoCloseSharp,IoColorPaletteSharp,IoSend } from "react-icons/io5";
 import { BsEmojiSmile } from "react-icons/bs";
+import { SiGradleplaypublisher } from "react-icons/si";
 import { useState } from "react";
 
 const bg_colors = ['#f44336','#e91e63','#9c27b0','#673ab7','#3f51b5','#2196f3','#009688','#4caf50','#8bc34a','#cddc39','#ffeb3b','#795548','#607d8b']
@@ -11,7 +12,7 @@ export const Text_status = ({set_status_type})=> {
 
     const Color_picker = ()=> {
         return (
-            <div className=" absolute right-0 top-full z-40">
+            <div className=" absolute right-0 top-full z-40 max-h-[350px] overflow-y-auto">
                {
                 bg_colors?.map((color)=> (
                     <button 
@@ -27,8 +28,8 @@ export const Text_status = ({set_status_type})=> {
 
 
     return (
-        <div style={{backgroundColor: bg_color}} className={`fixed z-40 left-0 top-0 w-dvw h-dvh  p-3`}>
-            <div className="flex items-center justify-between">
+        <div style={{backgroundColor: bg_color}} className={`fixed z-40 left-0 top-0 w-dvw h-dvh flex flex-col justify-between`}>
+            <header className="flex items-center justify-between p-3">
                 <button onClick={()=> set_status_type(null)} className="">
                     <IoCloseSharp size={26} />
                 </button>
@@ -46,7 +47,16 @@ export const Text_status = ({set_status_type})=> {
                         }
                     </div>
                 </div>
-            </div>
+            </header>
+            <footer className="bg-[#0000003a] h-20 flex items-center justify-between px-3">
+                <button className="flex items-center gap-1 bg-[#00000065] py-2 px-6 rounded-3xl">
+                    <SiGradleplaypublisher size={20} /> <span> Status (Status)</span>
+                </button>
+                <button className="bg-[#14752181] rounded-full p-4">
+                    <IoSend size={22} />
+                </button>
+
+            </footer>
         </div>
     )
 }
