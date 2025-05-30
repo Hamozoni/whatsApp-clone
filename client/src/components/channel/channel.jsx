64a6c71a0e22a9b0aff8057debc32b40155467b5
Mohamed Yahia <m.yahya199290@gmail.com>
2025-05-30 23:23:05 +0300

    modified:   client/src/components/channel/channel.jsx

--- client/src/components/channel/channel.jsx
+++ client/src/components/channel/channel.jsx
@@ -4,6 +4,12 @@ export const Channel = () => {
     return (
         <div className="flex h-dvh">
             <div className="p-3 flex-1 border-r border-r-gray-800 flex flex-col min-w-[350px] md:max-w-[380px] ">
+                <header className='border-b border-b-black'>
+                    <h5 className="text-lg font-bold mb-4">Settings</h5>
+                    <SearchInput handleSearch={()=> ''} text={text} setText={setText} />
+                </header>
+
+
             </div>
             <div className="hidden md:flex flex-2 items-center justify-center flex-col gap-5">
                 <GrChannel size={48} className="text-gray-400" />
