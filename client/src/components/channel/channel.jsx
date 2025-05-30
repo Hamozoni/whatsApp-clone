999fbd1e95087869cc08be387706f055b795480b
Mohamed Yahia <m.yahya199290@gmail.com>
2025-05-30 23:30:14 +0300

    modified:   client/src/components/channel/channel.jsx

--- client/src/components/channel/channel.jsx
+++ client/src/components/channel/channel.jsx
@@ -1,16 +1,25 @@
 import { useState } from "react";
 import { GrChannel } from "react-icons/gr";
 import {SearchInput} from '../ui/searchInput';
+import {RoundedBtn} from "../ui/roundedBtn";
+import { IoMdAdd } from "react-icons/io";
 
 export const Channel = () => {
 
-    const [text,setText] = useState('')
+    const [text,setText] = useState('');
+
+    const handleCreateChannel = ()=> [
+        
+    ]
 
     return (
         <div className="flex h-dvh">
             <div className="p-3 flex-1 border-r border-r-gray-800 flex flex-col min-w-[350px] md:max-w-[380px] ">
                 <header className='border-b border-b-black'>
-                    <h5 className="text-lg font-bold mb-4">Channels</h5>
+                    <div className="">
+                        <h5 className="text-lg font-bold mb-4">Channels</h5>
+                        <RoundedBtn Icon={IoMdAdd} onClick={handleCreateChannel} />
+                    </div>
                     <SearchInput handleSearch={()=> ''} text={text} setText={setText} />
                 </header>
 
