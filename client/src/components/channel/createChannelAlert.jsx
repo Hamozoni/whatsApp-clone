d6046860ffe69965d90ce1964840e21f6242bf03
Mohamed Yahia <m.yahya199290@gmail.com>
2025-06-01 19:04:12 +0300

    modified:   client/src/components/channel/createChannelAlert.jsx

--- client/src/components/channel/createChannelAlert.jsx
+++ client/src/components/channel/createChannelAlert.jsx
@@ -1,9 +1,31 @@
-
-
+import { GrChannel } from "react-icons/gr";
+import { GiWorld } from "react-icons/gi";
 export const CreateChannelAlert = ( )=> {
 
 
     return (
-        <div className=""></div>
+        <div className="">
+
+            <header>
+                <div className="">
+                    <GrChannel />
+                </div>
+                <h4>create a channel to reach unlimted followers</h4>
+            </header>
+            <div className="">
+                <div className="">
+                    <div className="">
+                        <GiWorld />
+                    </div>
+                    <div className="">
+                        <h5>Anyone can discover your channel</h5>
+                        <p>Channels are poblic, so anyone can find them and 30 dayes of history</p>
+                    </div>
+                </div>
+            </div>
+            <footer>
+                
+            </footer>
+        </div>
     )
 }
