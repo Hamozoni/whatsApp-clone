739c2ccff592aa277473f7974809df05a4c598a4
Mohamed Yahia <m.yahya199290@gmail.com>
2025-06-01 18:42:23 +0300

    modified:   server/src/models/channel.model.js

--- server/src/models/channel.model.js
+++ server/src/models/channel.model.js
@@ -2,9 +2,14 @@ import mongoose from "mongoose";
 
 
 const CHANNEL_SCHEMA = new mongoose.Schema({
-    onner : {type: mongoose.Schema.Types.ObjectId,ref: 'User'},
+    admins : [{type: mongoose.Schema.Types.ObjectId,ref: 'User'}],
+    followers : [{type: mongoose.Schema.Types.ObjectId,ref: 'User'}],
+    name: {type: String, require: true},
+    about: {type: String, require: true},
+    messages: [{type: mongoose.Schema.Types.ObjectId,ref: 'Message'}],
+    profile_picture: {type: String,require: true,default: '/placeholder_avatar.jpg'},
 
-});
+},{timestamps: true});
 
 
 const Channel = mongoose.model('Channel',CHANNEL_SCHEMA);
