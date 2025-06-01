ce730d2bc8a695aa815cc534137fecf6ccc2be4d
Mohamed Yahia <m.yahya199290@gmail.com>
2025-06-01 18:32:47 +0300

    modified:   server/src/models/channel.model.js

--- server/src/models/channel.model.js
+++ server/src/models/channel.model.js
@@ -3,5 +3,10 @@ import mongoose from "mongoose";
 
 const CHANNEL_SCHEMA = new mongoose.Schema({
     onner : {type: mongoose.Schema.Types.ObjectId,ref: 'User'},
-    
-})
+
+});
+
+
+const Channel = mongoose.model('Channel',CHANNEL_SCHEMA);
+
+export default Channel
