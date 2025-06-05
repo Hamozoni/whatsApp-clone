71707ccd85f3a7101e0285b9cfb7e0c4db7d6b6f
Mohamed Yahia <m.yahya199290@gmail.com>
2025-06-05 23:49:29 +0300

    modified:   server/src/routes/channel.route.js

--- server/src/routes/channel.route.js
+++ server/src/routes/channel.route.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { post_channel } from "../controllers/channel.controller.js";
+import { get_channel, post_channel } from "../controllers/channel.controller.js";
 import upload from "../middleware/upload_middleware.js";
 
 const router = Router();
 
-router.route('/').post(upload.single('file'),post_channel);
-
+router.route('/')
+.post(upload.single('file'),post_channel)
+.get(get_channel)
 
 export default router;
