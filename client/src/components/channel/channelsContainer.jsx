86908d731210234a5f800c33a356e3c916312ef4
Mohamed Yahia <m.yahya199290@gmail.com>
2025-06-06 19:28:45 +0300

    modified:   client/src/components/channel/channelsContainer.jsx

--- client/src/components/channel/channelsContainer.jsx
+++ client/src/components/channel/channelsContainer.jsx
@@ -1,5 +1,6 @@
 import { useContext } from "react"
 import { User_context } from "../../contexts/user.context"
+import { Avatar } from "../ui/avatar";
 
 
 export const ChannelsContainer = ()=> {
@@ -9,9 +10,19 @@ export const ChannelsContainer = ()=> {
     return (
         <div className="">
             {
-                channels?.map(()=> (
-                    <div className="">
-                        
+                channels?.map((channel)=> (
+                    <div key={channel?._id} className=" flex items-center gap-3">
+                        <Avatar size='lg' user_photo={channel.avatar.url}/>
+                        <div className="flex-1">
+                            <div className="flex items-center justify-between">
+                                <h4 className=" text-gray-100 text-lg">{channel?.name}</h4>
+                                <span className="text-gray-400 text-sm">7:30</span>
+                            </div>
+                            <div className="flex items-center justify-between">
+                                <p className=" line-clamp-1 text-gray-400 text-sm">يتعهد طالب الزيارة بصحة المعلومات المدونة ومغادرة الشخص المطلوب قبل انتهاء المدة المحددة للزيارة</p>
+                                <span className="bg-emerald-400 rounded-full w-5 h-5 flex justify-center items-center">5</span>
+                            </div>
+                        </div>
                     </div>
                 ))
             }
