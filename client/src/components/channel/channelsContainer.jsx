68aea6c67d550bdacc2af4a0b8b9cb294c16fd10
Mohamed Yahia <m.yahya199290@gmail.com>
2025-06-06 18:42:54 +0300

    client/src/components/channel/channelsContainer.jsx

---
+++ client/src/components/channel/channelsContainer.jsx
@@ -0,0 +1,18 @@
+import { useContext } from "react"
+import { User_context } from "../../contexts/user.context"
+
+
+export const ChannelsContainer = ()=> {
+
+    const {channels} = useContext(User_context);
+
+    return (
+        <div className="">
+            {
+                channels?.map(()=> (
+                    <div className=""></div>
+                ))
+            }
+        </div>
+    )
+}
