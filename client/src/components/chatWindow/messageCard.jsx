399276caa2638b4d9d3bc1ca2b3d199b552d2a95
Mohamed Yahia <m.yahya199290@gmail.com>
2025-06-17 15:40:22 +0300

    modified:   client/src/components/chatWindow/messageCard.jsx         modified:   client/src/components/shared/callNotificationCard.jsx

--- client/src/components/chatWindow/messageCard.jsx
+++ client/src/components/chatWindow/messageCard.jsx
@@ -1,51 +1,43 @@
-import { useEffect, useState } from "react";
 import { MediaMessageCard } from './mediaMessageCard'
 import { CallNotificationCard } from "../shared/callNotificationCard";
 import { timeFormat } from "../../lib/timeFormat";
 
 export const MessageCard = ({userId,message})=> {
 
-    const [isMyMessage,setIsMyMessage] = useState(false);
-
-    useEffect(()=> {
-        setIsMyMessage(message?.sender?._id === userId);
-    },[userId,message]);
+    const isMyMessage = message?.sender?._id === userId
 
     return (
-        <div
-            className={`flex ${isMyMessage ? 'justify-end' : 'justify-start'}`}
-        >
-            <div
-                className={`max-w-[65%] rounded-lg p-2 relative ${
+        <div className={`flex ${isMyMessage ? 'justify-end' : 'justify-start'}`}>
+            <div className={`max-w-[65%] rounded-lg p-2 relative ${
                     isMyMessage
                     ? 'bg-emerald-800  ml-12'
                     : 'bg-[#222e35] mr-12'
                 }`}
-            >
-            {
-                message.type === 'MEDIA' ?
-              <MediaMessageCard sender={message?.sender} file={message?.file}/>
-              : message.type === 'CALL' && 
-               <div className="">
-                   <CallNotificationCard call={message?.call} />
-                </div>
-            }
-            {
-               (message.type === 'MEDIA' || message?.file?.type === 'AUDIO' || message.type === 'CALL') ? '':
-              <p className="text-sm">
-                    { message?.file?.type === 'APPLICATION' ? message?.file?.name : message?.text}
-              </p>
-            }
-                <div className="flex items-center justify-end space-x-1 mt-1">
-                    <span className="text-[10px] text-gray-300 font-[100]">
-                        {timeFormat(message?.createdAt)}
-                    </span>
-                    {(isMyMessage && message.type !== 'CALL') &&(
-                    <span className={`${message?.status === 'READ' ? 'text-emerald-300' :''} text-[10px]`}>
-                        {message?.status === 'SENT' ? '✓' : '✓✓'}
-                    </span>
-                    )}
-                </div>
+                >
+                    {
+                        message.type === 'MEDIA' ?
+                            <MediaMessageCard sender={message?.sender} file={message?.file}/>
+                        : message.type === 'CALL' && 
+                            <div className="">
+                                <CallNotificationCard call={message?.call} />
+                            </div>
+                    }
+                    {
+                        (message.type === 'MEDIA' || message?.file?.type === 'AUDIO' || message.type === 'CALL') ? '':
+                        <p className="text-sm">
+                                { message?.file?.type === 'APPLICATION' ? message?.file?.name : message?.text}
+                        </p>
+                    }
+                    <div className="flex items-center justify-end space-x-1 mt-1">
+                        <span className="text-[10px] text-gray-300 font-[100]">
+                            {timeFormat(message?.createdAt)}
+                        </span>
+                        {(isMyMessage && message.type !== 'CALL') &&(
+                        <span className={`${message?.status === 'READ' ? 'text-emerald-300' :''} text-[10px]`}>
+                            {message?.status === 'SENT' ? '✓' : '✓✓'}
+                        </span>
+                        )}
+                    </div>
             </div>
         </div>
     )
--- client/src/components/shared/callNotificationCard.jsx
+++ client/src/components/shared/callNotificationCard.jsx
@@ -2,29 +2,36 @@ import { SlCallOut } from "react-icons/sl";
 import { SlCallIn } from "react-icons/sl";
 import { RiVideoUploadLine } from "react-icons/ri";
 import { RiVideoDownloadLine } from "react-icons/ri";
-import { useContext, useEffect, useState } from "react";
+import { useContext} from "react";
 import { User_context } from "../../contexts/user.context";
 
 export const CallNotificationCard = ({call})=> {
 
     const {user} = useContext(User_context);
-    const [isMissedCall,setIsMissedCall] = useState(false);
-
-    useEffect(()=> {
-        setIsMissedCall(call?.call_status === 'MISSED' && user?._id === call?.callee?._id)
-    },[]);
+    const isMissedCall = call?.call_status === 'MISSED' && user?._id === call?.callee?._id;
 
     return (
-        <div className={`flex items-center gap-1`}>
+        <div className={`flex items-center gap-2`}>
         {
               call?.type === 'AUDIO' ? 
             <>
               {
                 user?._id === call?.caller ? 
-                   <> <SlCallOut size={12} /> <span className="text-[#667781] capitalize text-sm font-medium">outgoing audio call</span>  </>
+                   <> 
+                      <SlCallOut size={18} /> 
+                      <h6 className="text-gray-400 capitalize text-sm font-medium">
+                        outgoing audio call
+                      </h6> 
+                    </>
                     :
-                   <> <SlCallIn size={12} className={isMissedCall && 'text-red-400'} />  
-                      <span className="text-[#667781] capitalize text-sm font-medium">{isMissedCall ? 'missed audio call' : 'incoming audio call'} </span>
+                   <> 
+                      <SlCallIn 
+                        size={18} 
+                        className={isMissedCall && 'text-red-400'}
+                        />  
+                      <h6 className="text-gray-400 capitalize text-sm font-medium">
+                        {isMissedCall ? 'missed audio call' : 'incoming audio call'} 
+                      </h6>
                    </>
                 } 
             </>
@@ -32,11 +39,21 @@ export const CallNotificationCard = ({call})=> {
             <>
               { 
                 user?._id === call?.caller ? 
-                   <> <RiVideoUploadLine size={20} /> <span className="text-[#667781] capitalize text-sm font-medium">outgoing video call</span>   </>
+                   <> 
+                      <RiVideoUploadLine size={20} /> 
+                      <h6 className="text-gray-400 capitalize text-sm font-medium">
+                        outgoing video call
+                      </h6>   
+                    </>
                     : 
                     <>
-                        <RiVideoDownloadLine size={20} className={isMissedCall && 'text-red-400'} /> 
-                         <span className="text-[#667781] capitalize text-sm font-medium">{isMissedCall ? 'missed video call' : 'incoming video call'} </span>
+                      <RiVideoDownloadLine 
+                          size={20} 
+                          className={isMissedCall && 'text-red-400'} 
+                      /> 
+                      <h6 className="text-gray-400 capitalize text-sm font-medium">
+                        {isMissedCall ? 'missed video call' : 'incoming video call'} 
+                      </h6>
                     </>
               } 
             </>
