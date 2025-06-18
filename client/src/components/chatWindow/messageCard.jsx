42e966356b0da880ce556401529bce3d48d43657
Mohamed Yahia <m.yahya199290@gmail.com>
2025-06-18 22:42:10 +0300

    modified:   client/src/components/chatWindow/messageCard.jsx

--- client/src/components/chatWindow/messageCard.jsx
+++ client/src/components/chatWindow/messageCard.jsx
@@ -8,36 +8,41 @@ export const MessageCard = ({userId,message})=> {
 
     return (
         <div className={`flex ${isMyMessage ? 'justify-end' : 'justify-start'}`}>
-            <div className={`max-w-[65%] rounded-lg p-2 relative ${
-                    isMyMessage
-                    ? 'bg-emerald-800  ml-12'
-                    : 'bg-[#222e35] mr-12'
-                }`}
-                >
+            <div className='max-w-[70%] mb-3' >
+                <div className={`rounded-lg min-w-12  ${
+                        isMyMessage
+                        ? 'bg-emerald-800'
+                        : 'bg-[#222e35]'
+                    }`}
+                   >
                     {
                         message.type === 'MEDIA' ?
-                            <MediaMessageCard sender={message?.sender} file={message?.file}/>
+                            <div className="p-1">
+                                <MediaMessageCard sender={message?.sender} file={message?.file}/>
+                            </div>
                         : message.type === 'CALL' && 
-                            <div className="">
+                            <div className="p-3 border-2 rounded-lg border-gray-500 cursor-pointer">
                                 <CallNotificationCard call={message?.call} />
                             </div>
                     }
                     {
                         (message.type === 'MEDIA' || message?.file?.type === 'AUDIO' || message.type === 'CALL') ? '':
-                        <p className="text-sm">
+                        <p className="text-sm p-2">
                                 { message?.file?.type === 'APPLICATION' ? message?.file?.name : message?.text}
                         </p>
                     }
-                    <div className="flex items-center justify-end space-x-1 mt-1">
-                        <span className="text-[10px] text-gray-300 font-[100]">
-                            {timeFormat(message?.createdAt)}
-                        </span>
-                        {(isMyMessage && message.type !== 'CALL') &&(
-                        <span className={`${message?.status === 'READ' ? 'text-emerald-300' :''} text-[10px]`}>
-                            {message?.status === 'SENT' ? '✓' : '✓✓'}
-                        </span>
-                        )}
-                    </div>
+
+                </div>
+                <div className={`flex items-center justify-end mt-1 ${isMyMessage ? 'justify-end' : 'justify-start'}`}>
+                    <span className="text-[10px] text-gray-300 font-[100]">
+                        {timeFormat(message?.createdAt)}
+                    </span>
+                    {(isMyMessage && message.type !== 'CALL') &&(
+                    <span className={`${message?.status === 'READ' ? 'text-emerald-300' :''} text-[10px]`}>
+                        {message?.status === 'SENT' ? '✓' : '✓✓'}
+                    </span>
+                    )}
+                </div>
             </div>
         </div>
     )
