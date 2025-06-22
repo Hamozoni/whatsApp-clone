cc42f4c54854cecaff52a770dd34e68b8885c414
Mohamed Yahia <m.yahya199290@gmail.com>
2025-06-22 12:46:36 +0300

    modified:   client/src/contexts/settings.context.jsx

--- client/src/contexts/settings.context.jsx
+++ client/src/contexts/settings.context.jsx
@@ -1,10 +1,25 @@
-import { createContext } from "react"
+import { createContext, useEffect, useState } from "react"
 
 export const SettingsContext = createContext(null);
 
 export const settingsContextProvider = ({children})=> {
+
+        const [theme,setTheme] = useState('dark');
+    
+        useEffect(()=> {
+            document.documentElement.classList.toggle('dark',theme === 'dark')
+        },[theme]);
+    
+        const toggleTheme = ()=> {
+            setTheme(prev=> prev === 'dark' ? 'light' : 'dark');
+        };
     return (
-        <SettingsContext.Provider>
+        <SettingsContext.Provider
+            value={{
+                theme,
+                toggleTheme,
+            }}
+            >
             {children}
         </SettingsContext.Provider>
     )
