297db8ac1deed9d050fed0048ffe9cb9214c4322
Mohamed Yahia <m.yahya199290@gmail.com>
2025-06-23 17:31:29 +0300

    modified:   client/src/components/modal/theme.jsx

--- client/src/components/modal/theme.jsx
+++ client/src/components/modal/theme.jsx
@@ -7,7 +7,7 @@ export const Theme = ()=> {
 
 
     return (
-        <div className="">
+        <div className="fixed z-50 left-1/2 top-1/2 -translate-1/2 rounded-lg bg-gray-800">
             <header>
                 <h5>theme</h5>
             </header>
@@ -19,11 +19,15 @@ export const Theme = ()=> {
                     <h6>light</h6>
                 </div>
                 <div className="">
-                    <button></button>
+                    <button className="rounded-full border-2 border-emerald-500 ">
+                        <span className="w-6 h-6 full border-2 border-gray-500 bg-emerald-500"></span>
+                    </button>
                      <h6>dark</h6>
                 </div>
                 <div className="">
-                    <button></button>
+                    <button className="rounded-full border-2 border-emerald-500 ">
+                        <span className="w-6 h-6 full border-2 border-gray-500 bg-emerald-500"></span>
+                    </button>
                      <h6>system default</h6>
                 </div>
             </div>
