2a4b982def3691a10678ebe38f0b6a980033eaff
Mohamed Yahia <m.yahya199290@gmail.com>
2025-06-23 18:21:35 +0300

    modified:   client/src/components/modal/theme.jsx

--- client/src/components/modal/theme.jsx
+++ client/src/components/modal/theme.jsx
@@ -8,9 +8,15 @@ export const Theme = ()=> {
 
     const ThemeBtn = ({name})=> {
         return (
-            <div className="flex gap-3 items-center">
-                <div className="rounded-full w-5 border-2 h-5 border-emerald-500 cursor-pointer">
-                    <div className="min-w-4 w-4 h-4 rounded-full border-3 border-gray-800 bg-emerald-500"></div>
+            <div onClick={()=> setTheme(name)}  className="flex gap-3 items-center cursor-pointer">
+                <div className="rounded-full w-5 border-2 h-5 border-emerald-500 ">
+                    {
+                        theme === name && (
+                            <div 
+                                className="min-w-4 w-4 h-4 rounded-full border-3 border-neutral-900 bg-emerald-500">
+                            </div>
+                        )
+                    }
                 </div>
                 <span className=" capitalize">
                     {name}
@@ -19,18 +25,18 @@ export const Theme = ()=> {
         )
     }
     return (
-        <div className="fixed z-50 left-1/2 top-1/2 w-[380px] max-w-screen -translate-1/2 rounded-lg bg-gray-800 p-5">
+        <div className="fixed z-50 left-1/2 top-1/2 w-[380px] max-w-screen -translate-1/2 rounded-lg bg-neutral-900 p-5">
             <header >
                 <h5>theme</h5>
             </header>
-            <div className="flex flex-col gap-5 my-5">
+            <div className="flex flex-col gap-6 my-5">
                  <ThemeBtn name='light' />
                  <ThemeBtn name='dark' />
-                 <ThemeBtn name='system default<' />
+                 <ThemeBtn name='system default' />
             </div>
             <footer className="flex items-center justify-end gap-5">
-                <button>cancel</button>
-                <button>ok</button>
+                <button className="text-gray-400 font-light">cancel</button>
+                <button className="bg-emerald-500 rounded-lg px-4 py-1">ok</button>
             </footer>
         </div>
     )
