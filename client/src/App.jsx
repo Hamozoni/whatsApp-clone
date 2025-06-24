2c1565d709060bc88cebfbd04a3d1d1b4882eab3
Mohamed Yahia <m.yahya199290@gmail.com>
2025-06-24 23:18:20 +0300

    modified:   client/src/App.jsx         modified:   client/src/main.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,7 +13,7 @@ function App() {
               <Route path='/signup' element={<Signup />} />
           </Routes>
         </BrowserRouter>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx';
 import { User_context_provider } from './contexts/user.context.jsx';
 import { Call_context_provider } from './contexts/call.context.jsx';
 import { ChatsContextProvider } from './contexts/chats.context.jsx';
-import {SettingsContextProvider} from './contexts/settings.context.jsx'
+import {SettingsContextProvider} from './contexts/settings.context.jsx';
 
 createRoot(document.getElementById('root')).render(
     <User_context_provider >
@@ -16,4 +16,4 @@ createRoot(document.getElementById('root')).render(
             </ChatsContextProvider>
         </SettingsContextProvider>
     </User_context_provider>
-)
+);
