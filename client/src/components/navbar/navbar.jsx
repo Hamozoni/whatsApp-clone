dc1e75613b159f1df4c47c13170d151d67a99668
Mohamed Yahia <m.yahya199290@gmail.com>
2025-07-01 19:31:37 +0300

    modified:   client/src/components/navbar/navbar.jsx         modified:   client/src/components/navbar/navbarIcon.jsx

--- client/src/components/navbar/navbar.jsx
+++ client/src/components/navbar/navbar.jsx
@@ -18,28 +18,37 @@ export const Navbar = ()=>  {
 
     const {user,activeNavbar,setActiveNavbar} = useContext(UserContext);
 
+
     return (
         <nav className="flex md:flex-col justify-between items-center md:px-2 md:py-4 p-1 bg-[#162127] m-1 rounded-lg" >
             <section className="flex md:flex-col gap-2">
                 {
                     options?.map(({Icon,text})=> (
-                        <NavbarIcon 
-                            Icon={Icon} 
-                            text={text} 
-                            key={text}
-                             />
+                            <NavbarIcon 
+                                key={text} 
+                                Icon={Icon} 
+                                text={text} 
+                                 />
                     ))
                 }
             </section >
             <section>
                 <hr className="text-[#394b55] my-5 hidden md:visible" />
                 <section className="flex md:flex-col gap-3 justify-center items-center" >
-                    <NavbarIcon Icon={IoSettingsOutline} text='settings' />
-                    <button 
-                        className={`border-2 rounded-full ${activeNavbar === 'profile' ? 'border-emerald-400' : 'border-transparent'}`}
-                        onClick={()=> setActiveNavbar('profile')}>
-                        <Avatar  userPhoto={user?.profile_picture} />
-                    </button>
+                    <NavBtnContainer text='settings'>
+                        <NavbarIcon 
+                            Icon={IoSettingsOutline} 
+                            text='settings' 
+                            />
+                    </NavBtnContainer>
+                    <div className="flex flex-col items-center justify-center">
+                        <button 
+                            className={`border-2 rounded-full ${activeNavbar === 'profile' ? 'border-emerald-400' : 'border-transparent'}`}
+                            onClick={()=> setActiveNavbar('profile')}>
+                            <Avatar  userPhoto={user?.profile_picture} />
+                        </button>
+                        <span className=" md:hidden text-gray-300 capitalize">profile</span>
+                    </div>
                 </section>
             </section>
         </nav>
--- client/src/components/navbar/navbarIcon.jsx
+++ client/src/components/navbar/navbarIcon.jsx
@@ -6,17 +6,24 @@ export const NavbarIcon = ({text,Icon})=> {
 
     const {activeNavbar,setActiveNavbar} = useContext(UserContext);
 
+
     return (
-        <button 
-            onClick={()=> setActiveNavbar(text)} 
-            className={`relative flex justify-center items-center p-1 rounded-md hover:opacity-85 cursor-pointer`}>
-            <Icon 
-                size={24} 
-                className={`${activeNavbar === text ? 'text-emerald-400' :'text-[#f7f8fa]'}`}/>
-            {
-                activeNavbar === text && 
-                <div className="absolute  bottom-[-2px] left-[25%] w-[50%] h-[2px] bg-emerald-400 rounded-full"></div>
-            }
-        </button>
+
+       <div className="flex flex-col items-center justify-center">
+            <button 
+                onClick={()=> setActiveNavbar(text)} 
+                className={`relative flex justify-center items-center p-1 rounded-md hover:opacity-85 cursor-pointer`}>
+                <Icon 
+                    size={24} 
+                    className={`${activeNavbar === text ? 'text-emerald-400' :'text-[#f7f8fa]'}`}/>
+                {
+                    activeNavbar === text && 
+                    <div className="absolute  bottom-[-2px] left-[25%] w-[50%] h-[2px] bg-emerald-400 rounded-full"></div>
+                }
+            </button>
+            <span className=" md:hidden text-gray-300 capitalize">
+                {text}
+            </span>
+        </div>
     )
     };
