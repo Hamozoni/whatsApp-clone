edd5e660b63ee5657c6fbb8df070022bf51a0d66
Mohamed Yahia <m.yahya199290@gmail.com>
2025-07-12 18:59:37 +0300

    modified:   client/package.json         modified:   client/src/index.css         modified:   client/src/main.jsx         modified:   client/tailwind.config.js

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,5 +1,5 @@
-import { createRoot } from 'react-dom/client';
 import './index.css';
+import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import { UserContextProvider } from './contexts/user.context.jsx';
 import {CallContextProvider } from './contexts/call.context.jsx';
