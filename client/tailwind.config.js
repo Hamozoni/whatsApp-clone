63a445d0c1cb1cfd85cc4bdf980ac11edab01a99
Mohamed Yahia <m.yahya199290@gmail.com>
2025-07-12 19:14:28 +0300

    modified:   client/package-lock.json         modified:   client/package.json         modified:   client/src/index.css         modified:   client/tailwind.config.js

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,19 +3,7 @@ export default {
   darkMode : 'class',
   theme : {
     extend: {
-      colors: {
-        bg_primaray : '#111b21',
-        bg_secondary: '111b21',
-        text_primaray : 'var(--text-color-primaray)',
-        text_secondary: 'var(--text-color-secondary)',
-        dark : {
-            bg_primaray : '#111b21',
-            bg_secondary: '111b21',
-            text_primaray : 'var(--text-color-primaray)',
-            text_secondary: 'var(--text-color-secondary)',
-        }
-        
-      }
+
     }
   }
 
