91401f149a9deeac44bfaa1a68c51f2f84b5256a
Mohamed Yahia <m.yahya199290@gmail.com>
2025-11-15 12:30:21 +0300

    modified:   server/src/models/chat.model.js

--- server/src/models/chat.model.js
+++ server/src/models/chat.model.js
@@ -4,7 +4,8 @@ const CHAT_SCHEMA = new mongoose.Schema({
     users: [{type: mongoose.Schema.Types.ObjectId, ref: 'User'}], 
     lastMssage: { type: mongoose.Schema.Types.ObjectId, ref: 'Message' },
     messages: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Message' }],
-    deleetedFor: [{type: mongoose.Schema.Types.ObjectId, ref: 'User'}]
+    deleetedFor: [{type: mongoose.Schema.Types.ObjectId, ref: 'User'}],
+    isGroup: {type : Boolean, default : false}
   }, { timestamps: true});
 
   const Chat = mongoose.model('Chat',CHAT_SCHEMA);
