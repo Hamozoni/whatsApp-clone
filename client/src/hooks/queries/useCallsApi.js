4b074aa43428cef47f1811f611bb45b5eb34d671
Mohamed Yahia <m.yahya199290@gmail.com>
2025-11-18 16:41:08 +0300

    client/src/hooks/queries/useCallsApi.js

---
+++ client/src/hooks/queries/useCallsApi.js
@@ -0,0 +1,9 @@
+import { useQuery } from "@tanstack/react-query";
+import {getAllCalls} from "../../services/apiServices";
+
+export const useCall = ()=> {
+    return useQuery({
+        queryKey: ['calls'],
+        queryFn: ()=>  getAllCalls()
+    });
+};
--- server/src/controllers/status.controller.js
+++ server/src/controllers/status.controller.js
@@ -1,14 +1,10 @@
 
 import Status from '../models/status.model.js';
-import User from '../models/user.model.js';
-import { upload_file } from '../utils/upload_file.js';
-
 
 export const get_status = async (req,res,next)=> {
 
     const {_id,contacts} = req.user;
     try  {
-
         const status = await Status.find({onwer:{$in: [...contacts,_id]}})
         return res.status(200).json(status)
     }
