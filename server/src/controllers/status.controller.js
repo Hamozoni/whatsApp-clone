88349e639ac1607b1d56ddede55d94a37d16a6b2
Mohamed Yahia <m.yahya199290@gmail.com>
2025-11-18 14:39:10 +0300

    modified:   server/src/controllers/status.controller.js

--- server/src/controllers/status.controller.js
+++ server/src/controllers/status.controller.js
@@ -3,23 +3,14 @@ import Status from '../models/status.model.js';
 import User from '../models/user.model.js';
 import { upload_file } from '../utils/upload_file.js';
 
-export const post_status = async (req,res,next)=> {
-    try {
-
-    }
-    catch (error) {
-        next(error)
-    }
-};
 
 export const get_status = async (req,res,next)=> {
 
-    const user_id = req.query;
-
-
+    const {_id,contacts} = req.user;
     try  {
 
-
+        const status = await Status.find({onwer:{$in: [...contacts,_id]}})
+        return res.status(200).json(status)
     }
     catch (error) {
         next(error)
