e1764800a8218fd761ee9b916229e5293c6255ee
Mohamed Yahia <m.yahya199290@gmail.com>
2025-11-20 19:42:07 +0300

    modified:   server/src/models/user.model.js

--- server/src/models/user.model.js
+++ server/src/models/user.model.js
@@ -3,9 +3,9 @@ import mongoose from 'mongoose';
 const USER_SCHEMA = new mongoose.Schema({
     displayName: {type: String,require: true},
     email: {type: String,require: true,unique: true},
-    about: {type: String,require: false,default:'Hey there! I am using WhatsApp.'},
+    bio: {type: String,require: false,default:'Hey there! I am using WhatsApp.'},
     emailVerified:{type: Boolean, default: false },
-    photoURL: {type: String,require: true,default: '/placeholder_avatar.jpg'},
+    photoURL: {type: String,require: true,default: './placeholder_avatar.jpg'},
     photoURLId: {type: String,require: false},
     firebaseUid: {type: String,require: true,unique: true},
     lastLoginAt: {type: Date,require: true,default: Date.now()},
