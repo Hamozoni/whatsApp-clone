be4440a2775b1a5579a79f2a592111946440b740
Mohamed Yahia <m.yahya199290@gmail.com>
2025-12-12 11:58:20 +0300

    mobile/babel.config.js         mobile/components/mediaGallery/

--- mobile/app/chat/mediaGallery.jsx
+++ mobile/app/chat/mediaGallery.jsx
@@ -1,12 +1,40 @@
-import { View, Text } from 'react-native';
+import { Image, View, Text, TouchableOpacity } from 'react-native';
+import PagerView from 'react-native-pager-view';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import { useRouter } from 'expo-router';
+import ZoomableImage from '../../components/mediaGallery/zoomableImage';
 // import { CHATS } from '../constants/chats';
 export default function MediaGallery() {
-
+    const router = useRouter();
 
 
     return (
-        <View>
-            <Text>MediaGallery</Text>
-        </View>
+        <PagerView
+            zoomEnabled={true}
+            initialPage={0}
+            style={{
+                flex: 1,
+            }}>
+            {/* <View style={{ flex: 1, flexDirection: "column", justifyContent: "space-between" }}> */}
+            {/* <View style={{ backgroundColor: "black", opacity: 0.5, padding: 20, paddingTop: 40 }}>
+                    <View style={{ flexDirection: "row", alignItems: "center", }}>
+                        <TouchableOpacity onPress={() => { router.back() }}>
+                            <Ionicons name="chevron-back" size={28} color="white" />
+                        </TouchableOpacity>
+                        <View style={{ flexDirection: "row", flex: 1, justifyContent: "space-between", alignItems: "center" }}>
+                            <View style={{ justifyContent: "center", alignItems: "center", flex: 1 }}>
+                                <Text style={{ color: "white", fontSize: 16, fontWeight: "bold" }}>John Doe</Text>
+                                <Text style={{ color: "white", fontSize: 12 }}>12/12/2025,12:34</Text>
+                            </View>
+                            <Ionicons name="pencil" size={20} color="white" />
+                        </View>
+                    </View>
+                </View> */}
+            <ZoomableImage image={require("../../assets/images/pexels-nati-87264186-34295251.jpg")} />
+            {/* <View style={{ backgroundColor: "black", opacity: 0.5, padding: 20, paddingTop: 40 }}>
+                    <Text style={{ color: "white", fontSize: 16, fontWeight: "bold" }}>John Doe</Text>
+                </View> */}
+            {/* </View> */}
+        </PagerView>
     )
 }
---
+++ mobile/components/mediaGallery/zoomableImage.jsx
@@ -0,0 +1,79 @@
+import { Image, Dimensions } from "react-native";
+import { Gesture, GestureDetector } from "react-native-gesture-handler";
+import Animated, { useAnimatedStyle, useSharedValue, withSpring, withTiming } from "react-native-reanimated";
+
+
+const { width, height } = Dimensions.get("window");
+
+export default function ZoomableImage({ image }) {
+    const scale = useSharedValue(1);
+    const savedScale = useSharedValue(1);
+
+    const translateX = useSharedValue(0);
+    const translateY = useSharedValue(0);
+    const savedX = useSharedValue(0);
+    const savedY = useSharedValue(0);
+
+    const pinch = Gesture.Pinch()
+        .onUpdate((e) => {
+            scale.value = savedScale.value * e.scale;
+        })
+        .onEnd(() => {
+            savedScale.value = scale.value;
+
+            if (scale.value < 1) {
+                scale.value = withTiming(1);
+                savedScale.value = 1;
+            }
+        });
+
+    const pen = Gesture.Pan()
+        .onUpdate((e) => {
+            if (scale.value > 1) {
+                translateX.value = savedX.value + e.translationX;
+                translateY.value = savedY.value + e.translationY;
+            }
+        })
+        .onEnd(() => {
+            savedX.value = translateX.value;
+            savedY.value = translateY.value;
+        });
+
+    const doubleTap = Gesture.Tap()
+        .numberOfTaps(2)
+        .onEnd(() => {
+            if (scale.value > 1) {
+                scale.value = withSpring(1);
+                translateX.value = withSpring(0);
+                translateY.value = withSpring(0);
+                savedScale.value = 1;
+                savedX.value = 0;
+                savedY.value = 0;
+            } else {
+                scale.value = withSpring(2.5);
+                savedScale.value = 2.5;
+            }
+        });
+
+    const composedGesture = Gesture
+        .Simultaneous(pinch, pen, doubleTap);
+
+    const animatedStyle = useAnimatedStyle(() => ({
+        transform: [
+            { scale: scale.value },
+            { translateX: translateX.value },
+            { translateY: translateY.value },
+        ]
+    }))
+
+    return (
+        <GestureDetector gesture={composedGesture}>
+            <Animated.View style={{ width, height, justifyContent: "center", alignItems: "center" }}>
+                <Animated.Image
+                    source={image}
+                    style={[{ width, height, resizeMode: "contain" }, animatedStyle]}
+                />
+            </Animated.View>
+        </GestureDetector>
+    );
+}
