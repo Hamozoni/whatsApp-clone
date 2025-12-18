552a4f4004c4d90267af4b240ad843cabd1f38f4
Mohamed Yahia <m.yahya199290@gmail.com>
2025-12-19 00:10:53 +0300

    make server and mobile app saperated repo

--- hamoAppMobile/app/status/index.jsx
+++
@@ -1,9 +0,0 @@
-import { View } from "react-native";
-import { Text } from "react-native";
-export default function Status() {
-    return (
-        <View>
-            <Text>Status</Text>
-        </View>
-    );
-}
--- hamoAppServer/src/routes/call.route.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { getCalls } from "../controllers/call.controller.js";
-
-const router = Router();
-
-router.route('/')
-    .get(getCalls)
-
-export default router
